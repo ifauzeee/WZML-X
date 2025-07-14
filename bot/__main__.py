b5cbb11109b28164b9cc0ea456b925aea068ff29
Muhammad Ibnu Fauzi <167849886+ifauzeee@users.noreply.github.com>
2025-07-14 12:34:55 +0700

    Update __main__.py
    Signed-off-by: Muhammad Ibnu Fauzi <167849886+ifauzeee@users.noreply.github.com>

--- bot/__main__.py
+++ bot/__main__.py
@@ -365,7 +365,7 @@ async def main():
     bot.add_handler(MessageHandler(ytdlp.ytdl, filters=command(BotCommands.YtdlCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
     bot.add_handler(MessageHandler(mirror_leech.leech, filters=command(BotCommands.LeechCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
     bot.add_handler(MessageHandler(mirror_leech.qb_leech, filters=command(BotCommands.QbLeechCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
-    bot.add_handler(MessageHandler(ytdlp.ytdl_leech, filters=command(BotCommands.YtdlLeechCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
+    bot.add_handler(MessageHandler(ytdlp.ytdlleech, filters=command(BotCommands.YtdlLeechCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
     
     # Add remaining handlers from the .BAK file
     bot.add_handler(MessageHandler(clone.clone, filters=command(BotCommands.CloneCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
