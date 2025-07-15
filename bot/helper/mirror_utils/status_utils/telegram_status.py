802eea73656a595cb00b96617bf969b8899a08c7
Muhammad Ibnu Fauzi <167849886+ifauzeee@users.noreply.github.com>
2025-07-15 11:39:52 +0700

    Update telegram_status.py
    Signed-off-by: Muhammad Ibnu Fauzi <167849886+ifauzeee@users.noreply.github.com>

--- bot/helper/mirror_utils/status_utils/telegram_status.py
+++ bot/helper/mirror_utils/status_utils/telegram_status.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
-from time import time
 from bot.helper.ext_utils.bot_utils import (
+    EngineStatus,
     MirrorStatus,
     get_readable_file_size,
     get_readable_time,
-    get_progress_bar_string,
-    EngineStatus,
 )
 
+
 class TelegramStatus:
-    def __init__(self, obj, size, listener, gid, status="up"):
+    def __init__(self, obj, size, message, gid, status, upload_details):
         self.__obj = obj
         self.__size = size
         self.__gid = gid
         self.__status = status
-        self.listener = listener
-        self.message = listener.message
-
-    def progress_bar(self):
-        return get_progress_bar_string(self.progress())
+        self.upload_details = upload_details
+        self.message = message
 
     def processed_bytes(self):
-        return self.__obj.processed_bytes if hasattr(self.__obj, 'processed_bytes') else 0
+        return get_readable_file_size(self.__obj.processed_bytes)
 
     def size(self):
         return get_readable_file_size(self.__size)
@@ -32,25 +28,27 @@ class TelegramStatus:
         return MirrorStatus.STATUS_DOWNLOADING
 
     def name(self):
-        return self.listener.name
-
-    def gid(self) -> str:
-        return self.__gid
+        return self.__obj.name
 
     def progress(self):
         try:
-            return f"{round(self.processed_bytes() / self.__size * 100, 2)}%"
-        except:
-            return "0.0%"
+            progress_raw = self.__obj.processed_bytes / self.__size * 100
+        except Exception:
+            progress_raw = 0
+        return f"{round(progress_raw, 2)}%"
 
     def speed(self):
-        return f"{get_readable_file_size(self.__obj.speed)}/s" if hasattr(self.__obj, 'speed') else '0 B/s'
+        return f"{get_readable_file_size(self.__obj.speed)}/s"
 
     def eta(self):
         try:
-            return get_readable_time((self.__size - self.processed_bytes()) / self.__obj.speed)
-        except:
+            seconds = (self.__size - self.__obj.processed_bytes) / self.__obj.speed
+            return get_readable_time(seconds)
+        except Exception:
             return "-"
+
+    def gid(self) -> str:
+        return self.__gid
 
     def download(self):
         return self.__obj
